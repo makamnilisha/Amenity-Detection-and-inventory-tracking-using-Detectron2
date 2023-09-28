44605e8e3234a3d04c560cb1808148c15c55d05b
nilishamp <nilisha9@gmail.com>
2023-09-27 23:39:23 -0700

    Connect login page with landing page. Show username on landing page.

--- dbconnect.py
+++ dbconnect.py
@@ -1,14 +1,17 @@
 import mysql.connector
 
+
 def connect_db(host: str = "localhost",
                database: str = "AMENITY_MANAGEMENT",
                user: str = "root",
-               password: str = "1234567"):
+               password: str = "12345678",
+               port: int = "3306"):
     mydb = mysql.connector.connect(
         host=host,
-        database= database,
-        user=user ,
-        password=password
+        database=database,
+        user=user,
+        password=password,
+        port=port,
     )
     return mydb
 
